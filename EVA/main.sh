272952a15ddfb8e7dc0aa621c3339b2a17852209
Sugirthan Sivalingam <s.sivalingam@uni-bonn.de>
2021-02-13 21:56:44 +0100

    Update main.sh

--- EVA/main.sh
+++ EVA/main.sh
@@ -11,4 +11,4 @@ qsub -q all.q -N ALN_$2 -pe threaded 12 $BASEDIR/alignment.sh $1 $2
 
 # submits variant callling 
 # waits for alignment job (-hold_jid takes jobid)
-qsub -q all.q -N CALL_$2 -pe threaded 3 -hold_jid ALN_$2 $BASEDIR/variantcalling_modified_2.sh $1 $2 
+qsub -q all.q -N CALL_$2 -pe threaded 3 -hold_jid ALN_$2 $BASEDIR/variantcalling.sh $1 $2 
